fd7e8f3c1e4cfffeefb975e7e1723b036355b9f2
Roosemberth Palacios <roosemberth@gmail.com>
2013-07-14 03:31:01 +0000

    html welcome

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
+var response = fs.readFileSync('index.html');
+response = response.toString('utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(response);
 });
 
 var port = process.env.PORT || 5000;
